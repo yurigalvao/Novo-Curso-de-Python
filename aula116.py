1f118a0bd0986a326b3d8efe93ede7e4c84f022b
Yuri Galvão <ygalvao514@gmail.com>
2025-09-05 09:25:07 -0300

    Revisão dia 04/09

--- aula116.py
+++ aula116.py
@@ -1,33 +1,39 @@
 # Criando arquivos com Python + Context Manager with
-# Usamos a função open para abrir
-# um arquivo em Python (ele pode ou não existir)
+# Usamos a fun��o open para abrir
+# um arquivo em Python (ele pode ou n�o existir)
 # Modos:
-# r (leitura), w (escrita), x (para criação)
-# a (escreve ao final), b (binário)
+# r (leitura), w (escrita), x (para cria��o)
+# a (escreve ao final), b (bin�rio)
 # t (modo texto), + (leitura e escrita)
 # Context manager - with (abre e fecha)
-# Métodos úteis
+# M�todos �teis
 # write, read (escrever e ler)
-# writelines (escrever várias linhas)
+# writelines (escrever v�rias linhas)
 # seek (move o cursor)
 # readline (ler linha)
 # readlines (ler linhas)
-# Vamos falar mais sobre o módulo os, mas:
+# Vamos falar mais sobre o m�dulo os, mas:
 # os.remove ou unlink - apaga o arquivo
 # os.rename - troca o nome ou move o arquivo
-# Vamos falar mais sobre o módulo json, mas:
+# Vamos falar mais sobre o m�dulo json, mas:
 # json.dump = Gera um arquivo json
 # json.load
-caminho_arquivo = 'C:\\Users\\yurig\\OneDrive\\Área de Trabalho\\novo curso de python\\venv\\Scripts\\Activate.ps1' # caminho completo de arquivo
-caminho_arquivo += 'aula116.txt'
+#caminho_arquivo = 'C:\\Users\\yurig\\OneDrive\\�rea de Trabalho\\novo curso de python\\venv\\Scripts\\Activate.ps1' # caminho completo de arquivo
+#caminho_arquivo += 'aula116.txt'
 
 #arquivo = open(caminho_arquivo, 'w') abrir e criar o arquivo, passo o caminho do arquivo e abro e fecho o arquivo
 #arquivo.close()
 
-with open('aula116.txt', 'w+') as arquivo:  #abre e fecha o arquivo
-    arquivo.write('Linha 1\n')
-    arquivo.write('Linha 2\n')
-    print(arquivo.read())
+#with open('aula116.txt', 'w+') as arquivo:  #abre e fecha o arquivo
+#    arquivo.write('Linha 1\n')
+#    arquivo.write('Linha 2\n')
+#    print(arquivo.read())
 
-with open('aula116.txt', 'r') as arquivo:
-    print(arquivo.read())
+#with open('aula116.txt', 'r') as arquivo:
+#    print(arquivo.read())
+caminho_arquivo = 'aula116.txt'
+
+with open(caminho_arquivo, 'w+', encoding='utf-8') as arquivo:
+    arquivo.write('Atenção\n')
+    arquivo.write('Print 123')
+    
